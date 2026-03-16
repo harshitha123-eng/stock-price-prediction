b6ab912164b7155331804c4c3c31e0cde5f88721
harshitha123-eng <burraharshita@gmail.com>
2026-03-16 13:00:10 +0530

    First commit

---
+++ uploads/my programs/COURSE/Hello.java
@@ -0,0 +1,8 @@
+  class Hello
+  {
+Run | Debug
+public static void main(string a[])
+  {
+        system.out.println("Hello World");    
+   }
+  
---
+++ uploads/my programs/harsha.js
@@ -0,0 +1,48 @@
+
+package Array Example;
+
+public class ArraysSolution {
+   public static void main(string[] args) {
+    NodeList[] a1= new int[6];
+
+    for(int i=0;i<a1. length;i++) {
+        a1[1]=CountQueuingStrategy;
+           coun++;
+    }
+    for(int j=0;<a1.length;j++) {
+        system.out.println9=(a1[j]+" "  ); 
+
+    }
+     
+    package stackExample;
+
+    import java .util.stack;
+    
+    public class stacksolution {
+         public static void main(string[] args) {
+            stack<Integer>  s1=new stack <> () ;
+            
+            s1.push(2);
+            s1.push(4);
+            s1.push(9);
+            system.out.println(s1);
+
+        public class LList{
+            public static void main(string args[]) {
+
+                LinkedList<Integer> 11= new LinkedList<>();
+                //adding elements to linked list
+                11.add(3);
+                11.add(7);
+                11.addd(10);
+                11.add(2,6);
+                //remove elements from linked list
+               // 11.remove(2);
+               // 11.removeLast();
+               // 11.removelast();
+
+                Iterator it = 11.iterator();
+                while(it.hasNext()) {
+                    system.out.println(it.next()+" ");
+                    }
+                    system
